885c4afbcf532c4b544c145447e97582a68ac219
ralam-local <ronanalam@berkeley.edu>
2025-08-01 13:21:22 -0700

    install DebugDraw & finish mobius func

--- Scenes/path_3d.gd
+++ Scenes/path_3d.gd
@@ -1,44 +1,34 @@
 extends Path3D
 
 @export var points: int
-var count: int
 @export var radius: float
 @export var z_position: float
 @export var stripWidth: float
 @export var stripThickness: float
+@export var twists: int
 
-#var point_position_i: Vector3
 
 func _ready() -> void:
 	curve.clear_points()
 	generate_segments(points, radius)
-	
-	#for i in range(count+1):
-		#var point_position_i := radius*Vector3( cos(2*PI*i/count), sin(2*PI*i/count), -3.0/radius )
-		#
-		#curve.add_point( point_position_i+ (stripWidth * i/count)*Vector3.FORWARD )
-		#curve.set_point_tilt(i, PI * i/count)
-		#curve.set_point_position( i, point_position_i + (stripWidth * i/count)*Vector3.FORWARD )
-		
-		# Make duplicated point at the end, but with 0 tilt
-		# to make the automatic "closing" extrusion unflipped
-		#if i == (count - 1):
-			#curve.add_point( point_position_i )
-			#curve.set_point_tilt(i+1, curve.get_point_tilt(i))
-			#curve.set_point_position(i+1, point_position_i + (stripWidth * i/count)*Vector3.FORWARD )
+
 
 func generate_segments(num_segments: int, r: float) -> void:
-	for i in range(num_segments):
-		var point_position_i := r*Vector3( cos(2*PI*i/num_segments), sin(2*PI*i/num_segments), z_position/r )
-		var lateral_offset_i := (stripWidth * i/count)*Vector3.FORWARD
-		var tangent_direction_i := Vector3( -point_position_i.y, point_position_i.x, 0.0 )/r
-		var in_position_i := -(4.0/3.0) * r * tan(PI/2*num_segments) * tangent_direction_i + z_position*Vector3.BACK
-		var out_position_i := (4.0/3.0) * r * tan(PI/2*num_segments) * tangent_direction_i + z_position*Vector3.BACK
+	var handle_length := (4.0/3.0)*tan(PI/(2*num_segments))
+	
+	for i in range(num_segments+1):
+		var frac := float(i)/float(num_segments)
+		var point_position_i := Vector3( r*cos(2*PI*frac), r*sin(2*PI*frac), z_position )
+		var tangent_direction_i := Vector3( -point_position_i.y, point_position_i.x, 0.0 ) * (1/r)
+		var in_position_i := r * handle_length * -tangent_direction_i
+		var out_position_i := r * handle_length * tangent_direction_i
 		
-		curve.add_point(point_position_i + lateral_offset_i, in_position_i, out_position_i)
-		
-		print(i)
-		print(curve.get_point_position(i))
+		curve.add_point(point_position_i, in_position_i, out_position_i)
+		curve.set_point_tilt(i, twists * PI * i/num_segments)
 
 #func _process(delta: float) -> void:
 	#pass
+
+#func _physics_process(delta: float) -> void:
+	#curve.clear_points()
+	#generate_segments(points, radius)
