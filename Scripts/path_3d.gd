63e0919ae50260542ee30e2f72f20be6cf6c55c4
ralam-local <ronanalam@berkeley.edu>
2025-08-02 01:06:56 -0700

    testing movement along the strip surface

--- Scripts/path_3d.gd
+++ Scripts/path_3d.gd
@@ -1,5 +1,7 @@
 extends Path3D
 
+@onready var mobius_polygon: CSGPolygon3D = $mobius_polygon
+
 @export var points: int
 @export var radius: float
 @export var z_position: float
@@ -9,6 +11,8 @@ extends Path3D
 
 
 func _ready() -> void:
+	configure_polygon(mobius_polygon, stripWidth, stripThickness)
+	
 	curve.clear_points()
 	generate_segments(points, radius)
 
@@ -25,6 +29,16 @@ func generate_segments(num_segments: int, r: float) -> void:
 		
 		curve.add_point(point_position_i, in_position_i, out_position_i)
 		curve.set_point_tilt(i, twists * PI * i/num_segments)
+
+
+func configure_polygon(polygon: CSGPolygon3D, width: float, thickness: float):
+	var fill_shape := Array(mobius_polygon.polygon)
+	fill_shape[0] = Vector2(-width/2.0, thickness/2.0)
+	fill_shape[1] = Vector2(-width/2.0, -thickness/2.0)
+	fill_shape[2] = Vector2(width/2.0, -thickness/2.0)
+	fill_shape[3] = Vector2(width/2.0, thickness/2.0)
+	
+	polygon.set_polygon(PackedVector2Array(fill_shape))
 
 #func _process(delta: float) -> void:
 	#pass
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,29 +1,53 @@
 extends CharacterBody3D
 
+@onready var path: Path3D = $"../mobius_path"
+
 const SPEED := 2.0
 const JUMP_VELOCITY := 4.5
 const MOUSE_SENS := 1.0
 
+var inMenu := false
+
 var vert_projection: float
+
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
 func _unhandled_input(event: InputEvent) -> void:
 	### Rotate camera w/ mouse
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion and !inMenu:
 		rotation.y -= event.relative.x * MOUSE_SENS/180.0
 		rotation.y = wrapf(rotation.y, 0.0, 2*PI)
 		rotation.x -= event.relative.y * MOUSE_SENS/180.0
 		rotation.x = clamp(rotation.x, -PI/2, PI/4)
+	
+	### Handle mouse capture with ESC
+	if event.is_action_pressed('ui_cancel'):
+		if inMenu:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		inMenu = !inMenu
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(dt: float) -> void:
 	### Add gravity.
 	#if not is_on_floor():
 		#velocity += get_gravity() * delta
 	### Handle jump.
 	#if Input.is_action_just_pressed("jump"):# and is_on_floor():
 		#velocity.y = JUMP_VELOCITY
-
+	
+	## Movement along the mobius strip
+	var F_gravity: Vector3
+	var input_LR: float = Input.get_axis('left', 'right')
+	var F_input: Vector3
+	
+	#velocity += (F_gravity + F_input) * dt
+	
+	## 3D floating movement
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	vert_projection = Input.get_axis('lowering', 'raising')
 	var direction := (transform.basis * Vector3(input_dir.x, vert_projection, input_dir.y)).normalized()
